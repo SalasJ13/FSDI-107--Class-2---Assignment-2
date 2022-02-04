c1e8a907dd3ca74ff9a21b4a7907fb924c4d8a33
Salas <94945634+SalasJ13@users.noreply.github.com>
2022-02-04 12:22:47 -0800

    Add files via upload

---
+++ src/service/dataService.js
@@ -0,0 +1,76 @@
+var catalog = [
+
+    {   
+        id: "dfh351hd",
+        title: "Cheetos",
+        img: "p3.jpg",
+        price: 5.00,
+        category: "salty"
+    },
+
+    {
+        id: "ds4gsg4r5",
+        title: "Cheetos",
+        img: "p3.png",
+        price: 5.00,
+        category: "salty"
+    },
+
+    {
+        id: "sdg45ss5g6",
+        title: "Cheetos",
+        img: "p5.jpg",
+        price: 5.00,
+        category: "salty"
+    },
+
+    {
+        id: "gw5rwr5",
+        title: "Cheetos",
+        img: "p7.jpg",
+        price: 10.00,
+        category: "special edition"
+    },
+
+    {
+        id: "4hd5h54eh",
+        title: "Cheetos",
+        img: "p10.jpg",
+        price: 10.00,
+        category: "special edition"
+    },
+
+    {
+        id: "02ggww556",
+        title: "Cheetos",
+        img: "p12.jpg",
+        price:10.00,
+        category: "special edition"
+    },
+
+    
+    {
+        id: "d5gs65g",
+        title: "Cheetos",
+        img: "p13.jpg",
+        price: 20.00,
+        category: "sweet"
+    },
+
+    {
+        id: "df47df",
+        title: "Cheetos",
+        img: "p14.jpg",
+        price: 20.00,
+        category: "sweet"
+    },
+    
+    {
+        id: "sdg46r48",
+        title: "Cheetos",
+        img: "p16.png",
+        price: 20.00,
+        category: "sweet"
+    }
+
+]
